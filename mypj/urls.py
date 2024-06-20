c36fdf4be9a59e5320ee1aee66f851c89fc5efb2
ohthisis <saitnyeinnaings@gmail.com>
2024-06-20 15:59:18 +0630

    image added

--- mypj/urls.py
+++ mypj/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('',include("myapp.urls")),
 ]+static(settings.STATIC_URL ,document_root=settings.STATIC_ROOT)
 
-# urlpatterns +=staticfiles_urlpatterns()
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns +=staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
